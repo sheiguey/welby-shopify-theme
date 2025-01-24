b4303d7cff3ba0954141fbbbe0bc983276e71b83
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2025-01-24 15:07:20 +0100

    test

--- assets/application.js
+++ assets/application.js
@@ -1,5 +1,63 @@
 $(document).ready(function () {
-  $(document).on('click', 'quantity-button', function (event) {
-    alert('button clicked');
+  $(document).on('click', '.js-quantity-button', function (event) {
+    let $button = $(this);
+    let $form = $button.closest('form');
+    let $quantity = $form.find('.js-quantity-field');
+    let quantityValue = parseInt($quantity.val());
+    let max = $quantity.attr('max') ? parseInt($quantity.attr('max')) : null;
+
+    if (
+      $button.hasClass('plus') &&
+      (max === null || quantityValue + 1 <= max)
+    ) {
+      // do something for plus click
+      $quantity.val(quantityValue + 1).change();
+    } else if ($button.hasClass('minus')) {
+      $quantity.val(quantityValue - 1).change();
+    }
+  });
+
+  $(document).on('change', '.js-quantity-field', function (event) {
+    let $field = $(this);
+    let $form = $field.closest('form');
+    let $quantityText = $form.find('.js-quantity-text');
+    let shouldDisableMinus = parseInt(this.value) === 1;
+    let shouldDisablePlus =
+      parseInt(this.value) === parseInt($field.attr('max'));
+
+    let $minusButton = $form.find('.js-quantity-button.minus');
+    let $plusButton = $form.find('.js-quantity-button.plus');
+
+    $quantityText.text(this.value);
+
+    if (shouldDisablePlus) {
+      $plusButton.prop('disabled', true);
+    } else if ($plusButton.prop('disabled') === true) {
+      $plusButton.prop('disabled', false);
+    }
+
+    if (shouldDisableMinus) {
+      $minusButton.prop('disabled', true);
+    } else if ($minusButton.prop('disabled') === true) {
+      $minusButton.prop('disabled', false);
+    }
+  });
+
+  $(document).on('change', '.js-variant-radio', function (event) {
+    let $radio = $(this);
+    let $form = $radio.closest('form');
+    let max = $radio.attr('data-inventory-quantity');
+    let $quantity = $form.find('.js-quantity-field');
+    let $addToCartButton = $form.find('#add-to-cart-button');
+
+    if ($addToCartButton.prop('disabled') === true) {
+      $addToCartButton.prop('disabled', false);
+    }
+
+    $quantity.attr('max', max);
+
+    if (parseInt($quantity.val()) > max) {
+      $quantity.val(max).change();
+    }
   });
 });
